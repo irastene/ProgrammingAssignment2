5b0efb0e8e9e882edd373c20fbb6bfaba1ff8dd9
irastene <irma.rastene@gmail.com>
2014-11-22 18:25:32 +0200

    More meaningfull names for variables & example

--- cachematrix.R
+++ cachematrix.R
@@ -2,16 +2,18 @@
 ## that stores a invertible matrix and cache's the inverse of that matrix.
 
 
-## This function creates a special "matrix" object that can cache its inverse.
-makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+## This function creates a special "matrix" object that can cache its inverse. 
+## List containing four function (set matrix, get matrix, set inverse of the 
+## matrix, get inverse of the matrix) is returned.
+makeCacheMatrix <- function(A = matrix()) {
+    inv <- NULL
+    set <- function(B) {
+        A <<- B
+        inv <<- NULL
     }
-    get <- function() x
-    setinverse <- function(solve) m <<- solve
-    getinverse <- function() m
+    get <- function() A
+    setinverse <- function(invB) inv <<- invB
+    getinverse <- function() inv
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -20,17 +22,35 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## This function computes the inverse of the special "matrix" returned 
 ## by makeCacheMatrix function. If the inverse has already been calculated 
-## (and the matrix has not changed), then the cachesolve retrieve the inverse 
+## (and the matrix has not changed), then the cacheSolve retrieve the inverse 
 ## from the cache.The assumpton that the matrix supplied is always invertible 
 ## is done.
-cacheSolve <- function(x, ...) {
-    m <- x$getinverse()
-    if(!is.null(m)) {
+cacheSolve <- function(A) {
+    inv <- A$getinverse()
+    if(!is.null(inv)) {
         message("getting cached data")
-        return(m)
+        return(inv)
+    } else {
+        data <- A$get()
+        inv <- solve(data)
+        A$setinverse(inv)
+        return(inv)
     }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setinverse(m)
-    m
 }
+
+
+## Example
+# a <- matrix(c(1,0,1,2,4,0,3,5,6), 3, 3)
+# b <- matrix(c(3,0,2,2,4,0,3,6,6), 3, 3)
+#
+# mya <- makeCacheMatrix(a)
+# cacheSolve(mya)
+# cacheSolve(mya)
+#
+# myb <- makeCacheMatrix(b)
+# cacheSolve(myb)
+# cacheSolve(myb)
+#
+# myb$set(a)
+# cacheSolve(myb)
+
